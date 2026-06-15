dc0d3b621c2028d7270e9a07b9cb4a87728d9065
Sérgio Ricardo Vieira Torres Silva <working7.sergio@gmail.com>
2026-06-15 15:15:15 -0300

    CARGOS ATUALIZADOS

---
+++ config.py
@@ -0,0 +1,49 @@
+# ===================== TABELAS =====================
+DIARIAS = {
+    "Grupo I": {
+        "Nacional": {
+            "Brasília": 743.74,
+            "RJ/SP/Manaus/BH": 673.74,
+            "Demais Capitais": 561.45,
+            "Demais Localidades": 449.16
+        },
+        "Internacional": {
+            "EUA/Canadá/Europa": 320,
+            "África/Ásia/Oceania": 300,
+            "América Latina": 280
+        }
+    },
+    "Grupo II": {
+        "Nacional": {
+            "Brasília": 631.45,
+            "RJ/SP/Manaus/BH": 561.45,
+            "Demais Capitais": 449.16,
+            "Demais Localidades": 352.91
+        },
+        "Internacional": {
+            "EUA/Canadá/Europa": 288,
+            "África/Ásia/Oceania": 270,
+            "América Latina": 252
+        }
+    },
+    "Grupo III": {
+        "Nacional": {
+            "Brasília": 519.16,
+            "RJ/SP/Manaus/BH": 449.16,
+            "Demais Capitais": 352.91,
+            "Demais Localidades": 320.83
+        },
+        "Internacional": {
+            "EUA/Canadá/Europa": 256,
+            "África/Ásia/Oceania": 240,
+            "América Latina": 224
+        }
+    }
+}
+
+CARGOS_GRUPOS = {
+    "Grupo I": ["AE", "ASSI", "ASSP", "ASTTE", "AGG"],
+    "Grupo II": ["SUP-S", "ASEG", "SUP-1", "AEG-1"],
+    "Grupo III": ["Secretário(a) de Estado", "Secretário(a) Executivo(a)"]
+}
+
