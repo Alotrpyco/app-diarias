679eaa4bb3166e39e9ee3965e01f4e8891c42996
Sérgio Ricardo <working7.sergio@gmail.com>
2026-06-16 20:22:13 -0300

    Adiciona relação por extenso do Grupo III

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 DIARIAS = {
     "Grupo I": {
         "Nacional": {
-            "Brasília": 743.74,
+            "Brasília/DF": 743.74,
             "RJ/SP/Manaus/BH": 673.74,
             "Demais Capitais": 561.45,
             "Demais Localidades": 449.16,
@@ -16,10 +16,11 @@ DIARIAS = {
     },
     "Grupo II": {
         "Nacional": {
-            "Brasília": 631.45,
+            "Brasília/DF": 631.45,
             "RJ/SP/Manaus/BH": 561.45,
             "Demais Capitais": 449.16,
-            "Demais Localidades": 352.91
+            "Demais Localidades": 352.91,
+            "Dentro do Território Estadual": 138
         },
         "Internacional": {
             "EUA/Canadá/Europa": 288,
@@ -29,10 +30,11 @@ DIARIAS = {
     },
     "Grupo III": {
         "Nacional": {
-            "Brasília": 519.16,
+            "Brasília/DF": 519.16,
             "RJ/SP/Manaus/BH": 449.16,
             "Demais Capitais": 352.91,
-            "Demais Localidades": 320.83
+            "Demais Localidades": 320.83,
+            "Dentro do Território Estadual": 120.75
         },
         "Internacional": {
             "EUA/Canadá/Europa": 256,
@@ -43,7 +45,8 @@ DIARIAS = {
 }
 
 CARGOS_GRUPOS = {
-    "Grupo I": ["SE: Secretário(a) de Estado", "SEE: Secretário(a) Executivo(a) de Gestão Interna"],
+    "Grupo I": ["SE: Secretário(a) de Estado", "SEE: Secretário(a) Executivo(a) de Gestão Interna"
+    ],
 
     "Grupo II": ["CHG: Chefe de Gabinete", "AG: Assessor de Governança e Transparência",
                 "ASE-1: Assessor Especial", 
@@ -54,8 +57,10 @@ CARGOS_GRUPOS = {
                 "SUP-2: Superintendente de Planejamento e Prospecção de Negócios Turisticos",
                 "SUP-2: Superintendente de Infraestrutura Logistica Para o Turismo",
                 "SUP-2: Superintendência de Promoção e Marketing de Produtos e Destinos",
-                "GER: Gerente Executivo(a) Administrativo(a)", "GER: Gerente Executivo(a) de Planejamento, Orçamento, Finanças e Contabilidade",
-                "GER: Gerente Executivo(a) de Valorização de Pessoas", "GER: Gerente Executivo(a) de Tecnologia da Informação", 
+                "GER: Gerente Executivo(a) Administrativo(a)", 
+                "GER: Gerente Executivo(a) de Planejamento, Orçamento, Finanças e Contabilidade",
+                "GER: Gerente Executivo(a) de Valorização de Pessoas", 
+                "GER: Gerente Executivo(a) de Tecnologia da Informação", 
                 "GER: Gerente Comercial do Centro Cultural e de Exposições Ruth Cardoso",
                 "GER: Gerente Operacional do Centro Cultural e de Exposições Ruth Cardoso",
                 "GER: Gerente de Desenvolvimento Regional e Programas Turísticos",
@@ -63,8 +68,27 @@ CARGOS_GRUPOS = {
                 "GER: Gerência dos Prestadores de Serviços Turísticos", 
                 "GER: Gerêmcia de Promoção e Marketing do Mercado Local e Nacional",
                 "GER: Gerência de Promoção e Marketing do Mercado Internacional",
-                "GER: Gerência de Eventos"],
+                "GER: Gerência de Eventos"
+    ],
 
-    "Grupo III": ["AE ", "ASSI", "ASSP", "ASTTE", "AGG"]
+    "Grupo III": ["ASSC: Assessor de Comunicação",
+                  "AST-1: Assessor Técnico",
+                  "AST-1: Assessor Técnico de Aquisição",
+                  "AST-1: Assessor Técnico de Frota, Patrimônio, Almoxarifado e de Controle do Consumo Interno",
+                  "AST-1: Assessor Técnico de Planejamento e Orçamento",
+                  "AST-1: Assessor Técnico de Finanças e Contabilidade",
+                  "AST-1: Assessor Técnico de Infraestrutura de Tecnologia da Informação e Suporte ao Usuário",
+                  "AST-1: Assessor Técnico de Projetos Turísticos",
+                  "AST-1: Assessor Técnico dos prestadores de Seviços Turísticos",
+                  "AST-2: Assessor Técnico",
+                  "AST-2: Assessor Técnico de Desenvolvimento de Pessoas",
+                  "SUPE: Supervisor de Planejamento, Orçamento, Finanças e Contabilidade",
+                  "SUPE: Supervisor de Pesquisa e Gestão de Indicadores, Metas, e Resultados",
+                  "SUPE: Supervisor de Programas, Capacitação e Qualificação Turística",
+                  "SUPE: Supervisor de Articulação e Captação de Negócios Turísticos",
+                  "SUPE: Supervisor de Fomento aos Mercados Emissores",
+                  "SUPE: Supervisor de Mídias Digitais",
+                  "SUPE: Servisor Técnico de Eventos"
+    ]
 }
 
