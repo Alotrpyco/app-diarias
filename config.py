53d5debefe1b3d6d490d837dffcb5dc9bb967b89
Sérgio Ricardo Vieira Torres Silva <working7.sergio@gmail.com>
2026-06-16 15:11:53 -0300

    Relação por Extenso do Grupo I e II

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ DIARIAS = {
             "Brasília": 743.74,
             "RJ/SP/Manaus/BH": 673.74,
             "Demais Capitais": 561.45,
-            "Demais Localidades": 449.16
+            "Demais Localidades": 449.16,
+            "Dentro do Território Estadual": 172.50
         },
         "Internacional": {
             "EUA/Canadá/Europa": 320,
@@ -42,8 +43,28 @@ DIARIAS = {
 }
 
 CARGOS_GRUPOS = {
-    "Grupo I": ["AE", "ASSI", "ASSP", "ASTTE", "AGG"],
-    "Grupo II": ["SUP-S", "ASEG", "SUP-1", "AEG-1"],
-    "Grupo III": ["Secretário(a) de Estado", "Secretário(a) Executivo(a)"]
+    "Grupo I": ["SE: Secretário(a) de Estado", "SEE: Secretário(a) Executivo(a) de Gestão Interna"],
+
+    "Grupo II": ["CHG: Chefe de Gabinete", "AG: Assessor de Governança e Transparência",
+                "ASE-1: Assessor Especial", 
+                "ASE-2: Assessor Especial",
+                "ASE-3: Assessor Especial",
+                "SUP-2: Superintendente Administrativo(a), de Planejamento, Finanças e Contabilidade",
+                "SUP-2: Superintendente do Centro Cultural e de Exposições Ruth Cardoso",
+                "SUP-2: Superintendente de Planejamento e Prospecção de Negócios Turisticos",
+                "SUP-2: Superintendente de Infraestrutura Logistica Para o Turismo",
+                "SUP-2: Superintendência de Promoção e Marketing de Produtos e Destinos",
+                "GER: Gerente Executivo(a) Administrativo(a)", "GER: Gerente Executivo(a) de Planejamento, Orçamento, Finanças e Contabilidade",
+                "GER: Gerente Executivo(a) de Valorização de Pessoas", "GER: Gerente Executivo(a) de Tecnologia da Informação", 
+                "GER: Gerente Comercial do Centro Cultural e de Exposições Ruth Cardoso",
+                "GER: Gerente Operacional do Centro Cultural e de Exposições Ruth Cardoso",
+                "GER: Gerente de Desenvolvimento Regional e Programas Turísticos",
+                "GER: Gerente de Projetos Turísticos", 
+                "GER: Gerência dos Prestadores de Serviços Turísticos", 
+                "GER: Gerêmcia de Promoção e Marketing do Mercado Local e Nacional",
+                "GER: Gerência de Promoção e Marketing do Mercado Internacional",
+                "GER: Gerência de Eventos"],
+
+    "Grupo III": ["AE ", "ASSI", "ASSP", "ASTTE", "AGG"]
 }
 
