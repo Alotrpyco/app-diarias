48574bac545f0c3858e338c17383775e4d2bab3a
Sérgio Ricardo <working7.sergio@gmail.com>
2026-06-18 01:39:10 -0300

    tipo: adicionar links úteis no rodapé da interface

---
+++ src/links.py
@@ -0,0 +1,14 @@
+import webbrowser
+
+def abrir_site_setur():
+    webbrowser.open("https://www.setur.al.gov.br/"
+                    )
+
+def abrir_banco_central():
+    webbrowser.open("https://www.bcb.gov.br/"
+                    )
+
+def abrir_cotacao_bcb():
+    webbrowser.open(
+        "https://www.bcb.gov.br/estabilidadefinanceira/historicocotacoes"
+        )
