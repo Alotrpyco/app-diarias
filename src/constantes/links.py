5fb333d5e40453d9dc3ec132954e60a064f7faee
Sérgio Ricardo <working7.sergio@gmail.com>
2026-06-20 16:06:41 -0300

    tipo: adicinado função para abrir decreto de diárias e app.py para a equipe

--- src/constantes/links.py
+++ src/constantes/links.py
@@ -12,3 +12,8 @@ def abrir_cotacao_bcb():
     webbrowser.open(
         "https://www.bcb.gov.br/estabilidadefinanceira/historicocotacoes"
         )
+
+def abrir_decreto():
+    webbrowser.open(
+        "https://leisestaduais.com.br/al/decreto-n-90173-2023-alagoas-regulamenta-a-concessao-de-diarias-aos-servidores-publicos-civis-do-poder-executivo-estadual-e-da-outras-providencias"
+        )  
---
+++ src/interface/app.py
@@ -0,0 +1,38 @@
+import customtkinter as ctk
+from tkinter import messagebox
+from PIL import Image
+from datetime import datetime
+import webbrowser
+import logging 
+
+
+from constantes.config import (
+    DIARIAS,
+    CARGOS_GRUPOS
+)
+
+from utils.formatadores import(
+    calcular_dias_diaria, 
+    formatar_moeda
+)
+
+
+#============================================================
+"""""
+====SISTEMA DE AUDITORIA (LOGGING)=====
+Responsável por registrar eventos importantes do sistema,
+como cálculos realizados, geração e erros.
+Os registros são armazenados em um arquivo .log para rastreabilidade.
+"""""
+
+logging.basicConfig(
+    filename="auditoria_diarias.log",
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    datefmt="%d/%m/%Y %H:%M:%S"
+)
+
+def log_auditoria(mensagem):
+    logging.info(mensagem)
+#===========================================================
+
