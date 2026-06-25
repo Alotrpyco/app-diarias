292eebb3edd7ac63f9b099a3bc1448f1b96c6317
Sérgio Ricardo Vieira Torres Silva <working7.sergio@gmail.com>
2026-06-25 12:29:44 -0300

    update

--- src/utils/formatadores.py
+++ src/utils/formatadores.py
@@ -1,25 +1,5 @@
 def formatar_moeda(valor):
     return f"{valor:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
 
-#=====CONDIÇÃO PARA O CALCULO DE DIÁRIAS======
-from datetime import datetime
-
-def calcular_dias_diaria(data_inicio, data_fim):
-    diferenca = data_fim - data_inicio
-    horas = diferenca.total_seconds() / 3600
-
-    if horas <= 12:
-        return 0.5, "½ diária (até 12h)", horas
-
-    dias = int(horas // 24)
-    resto = horas % 24
-
-    if resto > 12:
-        dias += 1
-        descricao = f"{dias} diárias"
-    else:
-        descricao = f"{dias} diárias + ½ meia diária"
-
-    return dias, descricao, horas
 
 
