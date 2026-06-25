1b289db5652a693aee14fce65dffb7af35a0f4d3
Sérgio Ricardo Vieira Torres Silva <working7.sergio@gmail.com>
2026-06-25 12:09:49 -0300

    tipo: corrigido erro de digitação

--- src/interface/app.py
+++ src/interface/app.py
@@ -17,9 +17,12 @@ from constantes.links import (
     abrir_decreto
 )
 
-from utils.formatadores import(
-    formatar_moeda,
+from utils.diarias import(
     calcular_dias_diaria
+)
+
+from utils.formatadores import(
+    formatar_moeda   
 )
 
 from utils.logger import log_auditoria
@@ -34,17 +37,17 @@ def iniciar_sistema():
     app.geometry("900x800")
 
 #=====================LOGO(SETUR)=========================
-logo = ctk.CTkImage(
-    light_image=Image.open("logo_setur.png"),
-    size=(140,140)
-)
+    logo = ctk.CTkImage(
+        light_image=Image.open("logo_setur.png"),
+        size=(140,140)
+    )
 
-ctk.CTkLabel(app, imagem=logo, text="").pack(pady=10)
+    ctk.CTkLabel(app, imagem=logo, text="").pack(pady=10)
 
-ctk.CTkLabel(
-    app, 
-    text="Secretaria de Estado do Turismo de Alagoas",
-    font=("Arial", 14)
-).pack(pady=(0,20))
-
+    ctk.CTkLabel(
+        app, 
+        text="Secretaria de Estado do Turismo de Alagoas",
+        font=("Arial", 14)
+    ).pack(pady=(0,20))
+    app.mainloop()
 
