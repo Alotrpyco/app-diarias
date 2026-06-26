ff569614b0997978f8512f4ecc2d5dc8f406afaf
Sérgio Ricardo <working7.sergio@gmail.com>
2026-06-25 22:05:18 -0300

    Update

--- src/interface/app.py
+++ src/interface/app.py
@@ -1,9 +1,9 @@
 from PIL import Image
 from datetime import datetime
+from tkinter import messagebox
 import webbrowser
 import logging 
 import customtkinter as ctk
-
 
 from constantes.config import (
     DIARIAS,
@@ -36,9 +36,9 @@ def iniciar_sistema():
     app.title("SETUR/AL - Sistema de Cálculo de Diárias")
     app.geometry("900x800")
 
-#=====================LOGO(SETUR)=========================
+#=====================LOGO DA SETUR======================
     logo = ctk.CTkImage(
-        light_image=Image.open("logo_setur.png"),
+        light_image=Image.open("image/logo_setur.png"),
         size=(140,140)
     )
 
@@ -49,5 +49,56 @@ def iniciar_sistema():
         text="Secretaria de Estado do Turismo de Alagoas",
         font=("Arial", 14)
     ).pack(pady=(0,20))
-    app.mainloop()
+    
+#========================QUADRO==========================
+    frame = ctk.CTkFrame(app)
+    frame.pack(fill="x", padx=20, pady=20)
 
+    localidade_var = ctk.StringVar()
+    grupo_var = ctk.StringVar(value="Grupo I")
+
+    ctk.CTkLabel(frame, text="Grupo").grid(row=0, column=0, padx=10, pady=10)
+
+    grupo_menu = ctk.CTkOptionMenu(
+        frame,
+        variable=grupo_var,
+        values=list(DIARIAS.keys())
+    )
+    grupo_menu.grid(row=0, column=1)
+
+    def mostrar_cargos():
+        grupos =grupo_var.get()
+        cargos = "\n".join(CARGOS_GRUPOS[grupos])
+        
+        messagebox.showinfo("Cargos do Grupo", cargos)
+
+    ctk.CTkButton(
+        frame,
+        text="Ver Cargos",
+        command=mostrar_cargos,
+        width=120
+    ).grid(row=0, column=2, padx=10)
+#=====================TIPO DE VIAGEM====================
+    ctk.CTkLabel(frame, text="Tipo").grid(row=2, column=0)
+
+    tipo_var = ctk.StringVar(value="Nacional")
+
+    tipo_menu = ctk.CTkOptionMenu(
+        frame,
+        variable=tipo_var,
+        values=["Nacional", "Internacional"]
+    )
+    tipo_menu.grid(row=1, column=1)
+
+#=================LOCALIDADE===========================
+    ctk.CTkLabel(frame, text="Localidade").grid(row=2, column=0)
+
+    localidade_menu = ctk.CTkOptionMenu(
+        frame,
+        variable=localidade_var,
+        values=[]
+    )
+    localidade_menu.grid(row=2, column=1)
+
+
+    
